85a3b0d8c926e2c611b5e3a2e9196aa2925a09a6
giovabubi <84048200+giovabubi@users.noreply.github.com>
2025-01-24 13:09:00 +0100

    Add files via upload

--- R/appost_script.R
+++ R/appost_script.R
@@ -4948,7 +4948,7 @@ Si vuole generare ugualmente i documenti dell'operatore economico per richiederl
     
     print(doc, target = paste0(pre.nome.file, "9 Dichiarazione assenza doppio finanziamento.docx"))
     
-    cat("\014")
+    #cat("\014")
     cat("
 
     Documento '", pre.nome.file, "9 Dichiarazione assenza doppio finanziamento.docx' generato e salvato in ", pat)
