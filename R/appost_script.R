412406faae3df223c7df096dcb206199a0b8342e
giovabubi <84048200+giovabubi@users.noreply.github.com>
2026-01-13 08:05:58 +0100

    Add files via upload

--- R/appost_script.R
+++ R/appost_script.R
@@ -2475,7 +2475,7 @@ Digitare il numero d'ordine e premere INVIO per caricare il file 'Ordini.csv' sc
       body_add_fpar(fpar(ftext("VISTO", fpt.b), ftext(" il d.lgs. 25 novembre 2016 n. 218, recante “Semplificazione delle attività degli enti pubblici di ricerca ai sensi dell'articolo 13 della legge 7 agosto 2015, n. 124”;")), style = "Normal") |>
       body_add_fpar(fpar(ftext("VISTA", fpt.b), ftext(" la legge 7 agosto 1990, n. 241 recante “Nuove norme in materia di procedimento amministrativo e di diritto di accesso ai documenti amministrativi” pubblicata sulla Gazzetta Ufficiale n. 192 del 18/08/1990 e s.m.i.;")), style = "Normal") |>
       body_add_fpar(fpar(ftext("VISTO", fpt.b), ftext(" lo Statuto del Consiglio Nazionale delle Ricerche, emanato con provvedimento del Presidente n. 93, prot. n. 0051080 del 19 luglio 2018, entrato in vigore in data 1° agosto 2018;")), style = "Normal") |>
-      body_add_fpar(fpar(ftext("VISTO", fpt.b), ftext(" il Regolamento di Organizzazione e Funzionamento del Consiglio Nazionale delle Ricerche - DPCNR nr. 119 Prot. n. 241776 del 10/07/2024, in vigore dal 1° agosto 2024;")), style = "Normal") |>
+      body_add_fpar(fpar(ftext("VISTO", fpt.b), ftext(" il Regolamento di Organizzazione e Funzionamento del Consiglio Nazionale delle Ricerche - DPCNR nr. 144 Prot. n. 521963 del 19 dicembre 2025, in vigore dal 1° gennaio 2026;")), style = "Normal") |>
       body_add_fpar(fpar(ftext("VISTO", fpt.b), ftext(" il Regolamento di amministrazione contabilità e finanza, emanato con Provvedimento della Presidente n. 201 prot. n. 0507722 del 23 dicembre 2024, entrato in vigore dal 1° gennaio 2025 ed, in particolare, l’art.32 rubricato “Decisione di contrattare”;")), style = "Normal") |>
       body_add_fpar(fpar(ftext("VISTO", fpt.b), ftext(" il D.lgs. 31 marzo 2023, n. 36 rubricato “Codice dei Contratti Pubblici in attuazione dell’articolo 1 della legge 21 giugno 2022, n. 78, recante delega al Governo in materia di contratti pubblici”, pubblicato sul Supplemento Ordinario n. 12 della GU n. 77 del 31 marzo 2023 (nel seguito per brevità “Codice”);")), style = "Normal") |>
       body_add_fpar(fpar(ftext("VISTO", fpt.b), ftext(" il D.lgs. 31 dicembre 2024, n. 209 rubricato “Disposizioni integrative e correttive al codice dei contratti pubblici, di cui al decreto legislativo 31 marzo 2023, n. 36”, pubblicato sul Supplemento Ordinario n.45/L della GU n. 305 del 31 dicembre 2024;")), style = "Normal") |>
@@ -2623,7 +2623,7 @@ Digitare il numero d'ordine e premere INVIO per caricare il file 'Ordini.csv' sc
 
     doc <- doc |>
       #body_add_fpar(fpar(ftext("CONSIDERATO", fpt.b), ftext(" che ai sensi dell’art. 53 comma 1 del Codice non sussistono particolari ragioni per la richiesta di garanzia provvisoria;")), style = "Normal") |>
-      body_add_fpar(fpar(ftext("VISTO", fpt.b), ftext(" il Bilancio Unico di Previsione del Consiglio Nazionale delle Ricerche per l’esercizio finanziario 2025, approvato dal Consiglio di Amministrazione con deliberazione n° 420/2024 del 17/12/2024;")), style = "Normal") |>
+      body_add_fpar(fpar(ftext("VISTO", fpt.b), ftext(" il Bilancio Unico di Previsione del Consiglio Nazionale delle Ricerche per l’esercizio finanziario 2026, approvato dal Consiglio di Amministrazione con deliberazione n. 245/2025 – Verbale 527 del 17 dicembre 2025;")), style = "Normal") |>
       body_add_fpar(fpar(ftext("VERIFICATA", fpt.b), ftext(" la disponibilità economico-finanziaria per la copertura della spesa sul budget del progetto “"),
                          ftext(Progetto),
                          ftext("”"),
@@ -2797,7 +2797,7 @@ Digitare il numero d'ordine e premere INVIO per caricare il file 'Ordini.csv' sc
       body_add_fpar(fpar(ftext("VISTO", fpt.b),
                          ftext(" lo Statuto del Consiglio Nazionale delle Ricerche, emanato con provvedimento del Presidente n. 93, prot. n. 0051080 del 19 luglio 2018, entrato in vigore in data 1° agosto 2018;")), style = "Normal") |>
       body_add_fpar(fpar(ftext("VISTO", fpt.b),
-                         ftext(" il Regolamento di Organizzazione e Funzionamento del Consiglio Nazionale delle Ricerche - DPCNR n. 119 prot. n. 241776 del 10 luglio 2024, entrato in vigore dal 1° agosto 2024;")), style = "Normal") |>
+                         ftext(" il Regolamento di Organizzazione e Funzionamento del Consiglio Nazionale delle Ricerche - DPCNR n. 144 Prot. n. 521963 del 19 dicembre 2025, in vigore dal 1° gennaio 2026;")), style = "Normal") |>
       body_add_fpar(fpar(ftext("VISTO", fpt.b),
                          ftext(" il Regolamento di amministrazione contabilità e finanza, emanato con Provvedimento della Presidente n. 201 prot. n. 0507722 del 23 dicembre 2024, entrato in vigore dal 1° gennaio 2025;")), style = "Normal") |>
       body_add_fpar(fpar(ftext("VISTO", fpt.b),
@@ -2815,7 +2815,7 @@ Digitare il numero d'ordine e premere INVIO per caricare il file 'Ordini.csv' sc
     
     doc <- doc |>
       body_add_fpar(fpar(ftext("VISTO", fpt.b),
-                         ftext(" il Bilancio Unico di Previsione del Consiglio Nazionale delle Ricerche per l’esercizio finanziario 2025, approvato dal Consiglio di Amministrazione con deliberazione n. 420/2024 – Verbale 511 del 17/12/2024;")), style = "Normal") |>
+                         ftext(" il Bilancio Unico di Previsione del Consiglio Nazionale delle Ricerche per l’esercizio finanziario 2026, approvato dal Consiglio di Amministrazione con deliberazione n. 245/2025 – Verbale 527 del 17 dicembre 2025;")), style = "Normal") |>
       body_add_fpar(fpar(ftext("VISTA", fpt.b), ftext(" la Richiesta di Acquisto prot. "),
                          ftext(Prot..RAS), ftext(" "),
                          ftext(della.fornitura), ftext(" di “"),
@@ -2906,7 +2906,7 @@ Digitare il numero d'ordine e premere INVIO per caricare il file 'Ordini.csv' sc
       body_add_fpar(fpar(ftext("VISTO", fpt.b),
                          ftext(" il d.lgs. 25 novembre 2016 n. 218, recante “Semplificazione delle attività degli enti pubblici di ricerca ai sensi dell'articolo 13 della legge 7 agosto 2015, n. 124”;")), style = "Normal") |>
       body_add_fpar(fpar(ftext("VISTO", fpt.b),
-                         ftext(" il Regolamento di Organizzazione e Funzionamento del CNR emanato con Provvedimento del Presidente nr. 119 Prot. n. 241776 del 10/07/2024, in vigore dal 01/08/2024;")), style = "Normal") |>
+                         ftext(" il Regolamento di Organizzazione e Funzionamento del CNR emanato con Provvedimento del Presidente nr. 144 Prot. n. 521963 del 19 dicembre 2025, in vigore dal 1° gennaio 2026;")), style = "Normal") |>
       body_add_fpar(fpar(ftext("VISTO", fpt.b),
                          ftext(" il Regolamento di Amministrazione Contabilità e Finanza (RACF) del Consiglio Nazionale delle Ricerche, emanato con provvedimento della Presidente CNR n. 201 del 23 dicembre 2024, in vigore dal 1° gennaio 2025;")), style = "Normal") |>
       body_add_fpar(fpar(ftext("VISTO", fpt.b),
@@ -4284,7 +4284,7 @@ Digitare il numero d'ordine e premere INVIO per caricare il file 'Ordini.csv' sc
       body_add_fpar(fpar(ftext("VISTO", fpt.b),
                          ftext(" lo Statuto del Consiglio Nazionale delle Ricerche, emanato con provvedimento del Presidente n. 93, prot. n. 0051080 del 19 luglio 2018, entrato in vigore in data 1° agosto 2018;")), style = "Normal") |>
       body_add_fpar(fpar(ftext("VISTO", fpt.b),
-                         ftext(" il Regolamento di Organizzazione e Funzionamento del Consiglio Nazionale delle Ricerche - DPCNR n. 119 prot. n. 241776 del 10 luglio 2024, entrato in vigore dal 1° agosto 2024;")), style = "Normal") |>
+                         ftext(" il Regolamento di Organizzazione e Funzionamento del Consiglio Nazionale delle Ricerche - DPCNR n. 144 Prot. n. 521963 del 19 dicembre 2025, in vigore dal 1° gennaio 2026;")), style = "Normal") |>
       body_add_fpar(fpar(ftext("VISTO", fpt.b),
                          ftext(" il Regolamento di amministrazione contabilità e finanza, emanato con Provvedimento della Presidente n. 201 prot. n. 0507722 del 23 dicembre 2024, entrato in vigore dal 1° gennaio 2025;")), style = "Normal") |>
       body_add_fpar(fpar(ftext("VISTO", fpt.b),
@@ -4307,7 +4307,7 @@ Digitare il numero d'ordine e premere INVIO per caricare il file 'Ordini.csv' sc
       # body_add_fpar(fpar(ftext("VISTO", fpt.b),
       #                    ftext(" il provvedimento del Direttore Generale (prot. 502457 del 18/12/2024) di proroga operativa delle funzioni di Segretario Amministrativo abilitato alla firma degli ordinativi finanziari e del controllo interno di regolarità amministrativo-contabile delle strutture dell’Ente nelle more del conferimento delle nomine a Responsabili della Gestione e della Compliance amministrativo contabile (RGC);")), style = "Normal") |>
       body_add_fpar(fpar(ftext("VISTO", fpt.b),
-                         ftext(" il Bilancio Unico di Previsione del Consiglio Nazionale delle Ricerche per l’esercizio finanziario 2025, approvato dal Consiglio di Amministrazione con deliberazione n° 420/2024 – Verbale 511 del 17/12/2024;")), style = "Normal") |>
+                         ftext(" il Bilancio Unico di Previsione del Consiglio Nazionale delle Ricerche per l’esercizio finanziario 2026, approvato dal Consiglio di Amministrazione con deliberazione n. 245/2025 – Verbale 527 del 17 dicembre 2025;")), style = "Normal") |>
       body_add_fpar(fpar(ftext("VISTA", fpt.b),
                          ftext(" la richiesta d'acquisto prot. n. "),
                          ftext(Prot..RAS),
@@ -6033,7 +6033,9 @@ Digitare il numero d'ordine e premere INVIO per caricare il file 'Ordini.csv' sc
                            ftext(Fornitore, fpt.b),
                            ftext(" (P.IVA "),
                            ftext(Fornitore..P.IVA),
-                           ftext("), titolare dei fondi e responsabile del progetto nell'ambito del "),
+                           ftext("), "),
+                           ftext("titolare dei fondi e responsabile del progetto", fpt.b),
+                           ftext(" nell'ambito del "),
                            ftext(Progetto.int),
                            ftext(", consapevole delle conseguenze penali di dichiarazioni mendaci, falsità in atti o uso di atti falsi, ai sensi dell’art. 76 D.P.R. 445/2000,")), style = "Normal")
     }
